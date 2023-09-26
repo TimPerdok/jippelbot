1988488d418086c3d43bbfa6bfff1776cc0e3e2b
Tim Perdok <tim-perdok@hotmail.com>
2023-09-26 18:15:05 +0200

    f

---
+++ src/commands/Summon.ts
@@ -0,0 +1,41 @@
+import { ButtonInteraction, ChatInputCommandInteraction, Client, Collection, DiscordAPIError, GuildChannelManager, Interaction, InteractionResponse, parseWebhookURL, SlashCommandBuilder, TextChannel } from "discord.js";
+import Command from "../classes/Command";
+import Poll from "../classes/Poll";
+import Classfinder from "../classes/Classfinder";
+import Subcommand from "../classes/Subcommand";
+import DataHandler from "../classes/datahandlers/DataHandler";
+import { PollJSON } from "../types/PollJSON";
+
+
+export default class Vote extends Command {
+
+
+    get data(): SlashCommandBuilder {
+        const builder = new SlashCommandBuilder()
+        .setName(this.name)
+        .setDescription(this.description)
+        .addUserOption(option => option.setName("user").setDescription("De persoon die je wilt summonen").setRequired(true))
+        return builder as SlashCommandBuilder;
+    }
+    
+    constructor() {
+        super("summon", "Summon iemand");
+        
+    }
+
+    
+    async onCommand(interaction: ChatInputCommandInteraction) {
+        const user = interaction.options.getUser("user");
+        await user.send(`Je wordt gesummoned door ${interaction.user.username} in ${interaction.guild.name}. Klik <#${interaction.channelId}> om te reageren.`);
+	}
+
+
+
+  
+
+    
+    
+    
+
+
+}
